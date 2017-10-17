951e0c71d54a63102d2ee8931c6623982bedd35b
Francisco Cerdán <wdev@outlook.es>
2017-10-17 22:19:00 +0200

    first commit

---
+++ Problems/Problem_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EulerSharp
+{
+	public class Problem_1
+	{
+		public Problem_1()
+		{
+			int result = 0;
+			for (int i = 1; i < 1000; i++)
+			{
+				if (i % 3 == 0 || i % 5 == 0)
+					result += i;
+			}
+			Console.WriteLine("Result: {0}", result);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EulerSharp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Problem_1 p = new Problem_1();
+		}
+	}
+}
